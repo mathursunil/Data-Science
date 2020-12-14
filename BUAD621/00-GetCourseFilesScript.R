db41e195418e7351dd0efbb51744dafc77ed74c7
mathursunil <mathsunil@gmail.com>
2020-12-14 08:31:36 -0500

    initial commit

---
+++ BUAD621/00-GetCourseFilesScript.R
@@ -0,0 +1,12 @@
+# Author : Sunil Mathur
+# This code is part of the course curriculum for MS - Data Science at University of Delaware
+# Purpose : get the course files from the server
+
+install.packages("usethis")
+# choose location for files by setting your working directory
+# to a folder you create with your OS ... like c:\analytics
+# use menus of RStudio:
+# Session --> Set Working Directory --> Choose Directory(i.e. analytics)
+courseURL = "https://github.com/flyaflya/buad621-2020/archive/master.zip"
+
+usethis::use_course(url = courseURL, destdir = getwd())
